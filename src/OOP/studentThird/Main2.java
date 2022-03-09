ef9b060f4e56451f063e15143dc8abc7f7ff8957
ZhenyaKrutoi <yevgeniy.meyntser@mail.ru>
2022-03-09 19:29:37 +0300

    Немного разграничил два кейса для видимости.

--- src/OOP/studentThird/Main2.java
+++ src/OOP/studentThird/Main2.java
@@ -8,9 +8,9 @@ public class Main2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Automibile Zheka = new Automibile("Компания Женька", 10);
-        int chooseAuto,choose, izmenenie, chet,search;
+        int choose, izmenenie, chet,search;
         float count, znachenie;
-        String name, searchString;
+        String chooseAuto,name, searchString;
         do {
             System.out.println("------------МЕНЮ-1-----------");
             System.out.println("1 - Добавить Автомобиль;");
@@ -22,11 +22,13 @@ public class Main2 {
             System.out.println("7 - Выход.");
             chet = 1;
 
-            chooseAuto = sc.nextInt();
+            chooseAuto = sc.next();
 
             switch (chooseAuto){
-                case 1 ->{
-                System.out.print("Введите Гос номер автомобиля: ");
+
+                case "1" ->{
+
+                    System.out.print("Введите Гос номер автомобиля: ");
                     name = sc.next();
                     ExThirdAuto Automobil = new ExThirdAuto(name);
                     Zheka.AddAuto(Automobil);
@@ -43,6 +45,7 @@ public class Main2 {
 
 
                         switch (choose) {
+
                             case 1 -> {
                                 System.out.println();
                                 System.out.println("Введите расход топлива для " + chet + " дня");
@@ -61,26 +64,33 @@ public class Main2 {
                                 Automobil.setRachod(znachenie, izmenenie);
                                 System.out.println();
                             }
+
                             case 3 -> {
                                 System.out.println("Введите новый Гос.номер автомобиля");
                                 searchString = sc.next();
                                 Automobil.setGosNumber(searchString);
                             }
+
                             case 4 -> System.out.println(Automobil.getData());
 
                         }
-                    }while (choose != 5) ;
 
+                    }while (choose != 5) ;
                     System.out.println(Zheka.getAutoCount());
                 }
-            case 2 -> {
+
+
+
+            case "2" -> {
                 System.out.print("\nВведите индекс автомобиля: ");
                 search = sc.nextInt();
                 System.out.println("\n" + Zheka.SearchAuto(search));
             }
-            case 3 -> System.out.println(Zheka.getDataCompany());
-            case 4 -> {
-                    float[] Massiv;
+
+            case "3" -> System.out.println(Zheka.getDataCompany());
+
+            case "4" -> {
+                float[] Massiv;
                 System.out.print("\nВведите индекс автомобиля: ");
                 search = sc.nextInt();
                 Massiv = Zheka.PoiskAuto(search);
@@ -94,7 +104,8 @@ public class Main2 {
                 }
                 System.out.println("\nВыполнено");
             }
-            case 5 -> {
+
+            case "5" -> {
                 System.out.print("\nВведите Гос.номер автомобиля: ");
                 String gosnom = sc.next();
                 Zheka.SearchAutoString(gosnom);
@@ -102,11 +113,13 @@ public class Main2 {
                 String Search2 = sc.next();
                 Zheka.AddAutoToGos(gosnom, Search2);
             }
-            case 6 ->{
+
+            case "6" ->{
                 System.out.println("\nВсего потрачено всеми автомобилями:");
                 System.out.println(Zheka.SumCompany());
             }
+
             }
-        } while (chooseAuto != 7);
+        } while (chooseAuto.equals("7") == false);
     }
 }
